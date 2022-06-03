4a8874c4b6819d78e8f5e470d805d5fd575534c5
solloc <matthias@maisch.me>
2022-06-03 21:53:50 +0200

    cleanup

--- access-random-file.ps1
+++ access-random-file.ps1
@@ -4,20 +4,10 @@ Param(
 )
 
 Write-Output "Accessing random file"
-# $files = Get-ChildItem -Path "./data" -Recurse -Attributes !Directory
-# $files = Get-ChildItem -Path "./data" -Recurse -File | Select-Object FullName
-# $files = Get-ChildItem -Path "./data" -Recurse -File
+
 $files = Get-ChildItem -Path $Path -Recurse -File
-# Start-Process -FilePath ".\data\f3\f31\f311\f3111\evangelion_by_machi_mile_df6mo2b-fullview.jpg"
-# $path = ".\data\f3\f31\f311\f3111\evangelion_by_machi_mile_df6mo2b-fullview.jpg"
-
-# Write-Output $files
-
 $randomFileIndex = Get-Random -Maximum $files.Count
 $filePath = $files[$randomFileIndex].FullName
-
-# $filePath = "D:\Workspaces\random-file-access\data\1=2.jpg"
-
 $filePath = """""""$($filePath)"""""""
 
 Write-Output "$randomFileIndex : $filePath"
