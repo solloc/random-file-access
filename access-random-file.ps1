73d3241427ca65301463d2faa8d126bce4a36cec
solloc <matthias@maisch.me>
2022-06-03 20:29:20 +0200

    random file selection

--- access-random-file.ps1
+++ access-random-file.ps1
@@ -1,5 +1,9 @@
 Write-Output "Accessing random file"
-Get-ChildItem -Path "./data" -Recurse -Attributes !Directory -Name
+$files = Get-ChildItem -Path "./data" -Recurse -Attributes !Directory -Name
 # Start-Process -FilePath ".\data\f3\f31\f311\f3111\evangelion_by_machi_mile_df6mo2b-fullview.jpg"
 $path = ".\data\f3\f31\f311\f3111\evangelion_by_machi_mile_df6mo2b-fullview.jpg"
-Invoke-Expression "explorer /select,$path"
+# Invoke-Expression "explorer /select,$path"
+Write-Output $files
+
+$randomFileIndex = Get-Random -Maximum $files.Count
+Write-Output $randomFileIndex
