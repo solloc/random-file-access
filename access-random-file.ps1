72ba21cd86431f9d38c12548bb611297243d9cfa
solloc <matthias@maisch.me>
2022-06-03 20:46:14 +0200

    path parameter added

--- access-random-file.ps1
+++ access-random-file.ps1
@@ -1,14 +1,20 @@
+Param(
+    [Parameter(Mandatory=$true)]
+    $Path
+)
+
 Write-Output "Accessing random file"
 # $files = Get-ChildItem -Path "./data" -Recurse -Attributes !Directory
 # $files = Get-ChildItem -Path "./data" -Recurse -File | Select-Object FullName
-$files = Get-ChildItem -Path "./data" -Recurse -File
+# $files = Get-ChildItem -Path "./data" -Recurse -File
+$files = Get-ChildItem -Path $Path -Recurse -File
 # Start-Process -FilePath ".\data\f3\f31\f311\f3111\evangelion_by_machi_mile_df6mo2b-fullview.jpg"
 # $path = ".\data\f3\f31\f311\f3111\evangelion_by_machi_mile_df6mo2b-fullview.jpg"
 
 # Write-Output $files
 
 $randomFileIndex = Get-Random -Maximum $files.Count
-$path = $files[$randomFileIndex].FullName
-Write-Output "$randomFileIndex : $path"
+$filePath = $files[$randomFileIndex].FullName
+Write-Output "$randomFileIndex : $filePath"
 
-Invoke-Expression "explorer /select,$path"
+# Invoke-Expression "explorer /select,$path"
